88ee13fbd08a2643a902a168e8ed60fa735c32a7
GuillaumeMCK <guillaume.mucwicki@gmail.com>
2026-03-01 21:58:17 +0100

    chore: Add input and output report exports

--- ffsds3/lib/ffsds3.dart
+++ ffsds3/lib/ffsds3.dart
@@ -1,5 +1,8 @@
 library;
 
 export 'package:usb_gadget/usb_gadget.dart';
+
 export 'src/device.dart';
 export 'src/inputs.dart';
+export 'src/reports/input_report.dart';
+export 'src/reports/output_report.dart';
